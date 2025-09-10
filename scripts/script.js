15118a7c3068262f34fafa9467008aa279cdb7fb
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-09-10 23:55:28 +0200

    Fixed initialization and imports

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
-import { podcasts } from "../scripts/data.js";
+import { podcasts } from "./data.js";                 // same folder
+import { ExportToMain } from "./components/createpodcast.js";
+
+
+function initialize() {
+
+  ExportToMain("podcastContainer");
+   
+
+}
+
+document.addEventListener("DOMContentLoaded", initialize);
 
 console.log(podcasts);
