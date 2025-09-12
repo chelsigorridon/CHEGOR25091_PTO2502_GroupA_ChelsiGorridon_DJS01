273c72f32e05c46b0de3a69041c0975f38519583
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-09-12 20:46:54 +0200

    Initialized openmodal & added import

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,18 @@
-import { podcasts } from "./data.js";                 // same folder
+import { podcasts,genres, seasons} from "./data.js";          
 import { ExportToMain } from "./components/createpodcast.js";
+import { openModal } from "./components/createmodal.js";
+
 
 
 function initialize() {
 
   ExportToMain("podcastContainer");
+  openModal("modal");
+  
    
 
 }
 
 document.addEventListener("DOMContentLoaded", initialize);
 
-console.log(podcasts);
+console.log(podcasts,genres,seasons);
