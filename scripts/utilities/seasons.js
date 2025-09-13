c4c106f640a957c40dba6b5bae208b757bf2f219
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-09-13 21:49:15 +0200

    Added seasons.js file

---
+++ scripts/utilities/seasons.js
@@ -0,0 +1,19 @@
+ import { seasons} from "../data.js"
+
+export function exportSeasons (podcastId, elementId) {
+  const seasons$ = document.getElementById(elementId);
+  seasons$.innerHTML = "";
+
+  const seasonsContainer = document.createElement("div");
+  seasonsContainer.className = "seasons_Container";
+
+  const seasonList = seasons
+      .filter(s => s.seasonDetails.includes(podcastId)) 
+      .map(s => s.title)
+      .join(", ");
+  
+    seasonsContainer.innerHTML = `<p>${seasonList}</p>`;
+  
+    seasons$.appendChild(seasonsContainer);
+  }
+   
