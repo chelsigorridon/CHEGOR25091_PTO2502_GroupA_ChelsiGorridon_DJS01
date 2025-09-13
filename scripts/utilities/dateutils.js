abb06fcae95aa150475e2aaee931910f50dbc6eb
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-09-13 20:31:01 +0200

    Converted to date to human readable format

---
+++ scripts/utilities/dateutils.js
@@ -0,0 +1,19 @@
+
+export function DateUtility(dateString, elementId) {
+  const date$ = document.getElementById(elementId);
+  date$.innerHTML = "";
+
+  const dateContainer = document.createElement("div");
+  dateContainer.className = "date_Container";
+
+  dateContainer.innerHTML = `
+    <p class="podcastDate">${new Date(dateString).toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  })}</p>
+  `;
+
+  date$.appendChild(dateContainer);
+}
