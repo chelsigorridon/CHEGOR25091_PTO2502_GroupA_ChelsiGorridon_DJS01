7a04041ab5d476742f5d6b7745bd7a322f458266
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-09-13 20:58:40 +0200

    Added Genre & Utility Import to main

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 import { podcasts,genres, seasons} from "./data.js";          
 import { ExportToMain } from "./components/createpodcast.js";
 import { openModal } from "./components/createmodal.js";
+import { DateUtility } from "./utilities/dateutils.js";
+import { exportGenre } from "./utilities/genreservice.js";
 
 
 
@@ -8,6 +10,9 @@ function initialize() {
 
   ExportToMain("podcastContainer");
   openModal("modal");
+  DateUtility("dateString")
+  exportGenre ("podcastId, elementId");
+  
   
    
 
