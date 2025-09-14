8f7ddb2e22864b9ab73095d8bd92879efce45105
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-09-14 16:24:26 +0200

    Fixed Genre function

--- scripts/utilities/genreservice.js
+++ scripts/utilities/genreservice.js
@@ -1,21 +1,18 @@
- import { genres} from "../data.js"
+import { genres } from "../data.js";
 
-export function exportGenre(podcastId) {
-  const genre$ = document.getElementById("`cardGenre-${podcastId}`");
+export function exportGenre(podcastId, elementId) {
+  const genre$ = document.getElementById(elementId);
   if (!genre$) return;
 
   const filteredGenres = genres.filter(g => g.shows.includes(podcastId));
 
-  
   const genreContainer = document.createElement("div");
   genreContainer.className = "genreContainer";
 
-  
   genreContainer.innerHTML = filteredGenres
     .map(g => `<span class="genre">${g.title}</span>`)
     .join(", ");
 
-  
   genre$.innerHTML = "";
-genre$.appendChild(genreContainer);
+  genre$.appendChild(genreContainer);
 }
